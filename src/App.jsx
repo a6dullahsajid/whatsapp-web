e4fa4cd6a6282b9fddf5da25a6d03aef80805e54
Abdullah Sajid <abdullahsajid1029@gmail.com>
2026-04-23 10:30:23 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className='bg-black text-white'>
+      hello
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f42c37",
+        secondary: "#f42c37",
+        brandYellow: "#fdc62e",
+        brandBlue: "#2dcc6f",
+        brandGreen: "#1376f4",
+        brandWhite: "#eeeeee",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
